0ace105826242827a101f0b7478768f06a292f27
Krayushkin68 <Krayuskhinml97@gmail.com>
2022-10-02 14:54:46 +0300

    added create post button

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="post-form" @submit.prevent>
-        <form class="form-control my-3">
+        <form class="form-control mb-3">
             <h4>Создание поста</h4>
             <div class="form-group mt-2">
                 <input type="text" class="form-control" id="title" placeholder="Название поста" v-model="post.title">
@@ -9,6 +9,7 @@
                 <input type="text" class="form-control" id="body" placeholder="Описание" v-model="post.body">
             </div>
             <button type="submit" class="btn btn-primary mb-2 mt-3" @click="CreatePost">Создать</button>
+            <button type="submit" class="btn btn-danger mb-2 mt-3 mx-2" @click="ClosePostForm">Отмена</button>
         </form>
     </div>
 </template>
@@ -32,6 +33,9 @@
                     title: '',
                     body: ''
                 }
+            },
+            ClosePostForm(){
+                this.$emit('close-post-form')
             }
         }
     }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,18 @@
 <template>
     <div class="container">
-        <div class="row">
-            <PostForm @create-post="CreatePost"/>
+        <div class="row align-items-end">
+            <div class="col col-9">
+                <PostForm v-if="isPostFormVisible" @create-post="CreatePost" @close-post-form="TogglePostForm"/>
+                <button v-else class="btn btn-primary mb-3" @click="TogglePostForm">Создать пост</button>
+            </div>
+            <div class="col col-3">
+                <select class="form-select mb-3" aria-label="Default select example">
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+            </div>
         </div>
         <div class="row">
             <PostList v-if="!isPostsLoading" :posts="posts" @delete-post="DeletePost"/>
@@ -11,7 +22,6 @@
 </template>
 
 <script>
-    // @ is an alias to /src
     import PostList from "@/components/PostList";
     import PostForm from "@/components/PostForm";
     import axios from 'axios';
@@ -25,16 +35,21 @@
         data() {
             return {
                 posts: [],
-                isPostsLoading: false
+                isPostsLoading: false,
+                isPostFormVisible: false
             }
         },
         methods: {
             CreatePost(post) {
                 this.posts.push(post);
+                this.isPostFormVisible = false;
             },
             DeletePost(post) {
                 console.log(post)
                 this.posts = this.posts.filter(p => p.id !== post.id)
+            },
+            TogglePostForm() {
+                this.isPostFormVisible = !this.isPostFormVisible
             },
             async fetchPosts() {
                 this.isPostsLoading = true;
