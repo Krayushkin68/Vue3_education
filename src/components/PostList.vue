ff5df87e7c6bba37429aa4566cc898dced3df188
Krayushkin68 <Krayuskhinml97@gmail.com>
2022-10-02 15:42:27 +0300

    added animation

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="posts">
-        <PostItem v-for="post in posts" :post="post" :key="post.id" @delete-post="DeletePost"/>
+        <transition-group name="posts-list">
+            <PostItem v-for="post in posts" :post="post" :key="post.id" @delete-post="DeletePost"/>
+        </transition-group>
     </div>
 </template>
 
@@ -27,5 +29,23 @@
 </script>
 
 <style scoped>
+    .posts-list-item {
+        display: inline-block;
+        margin-right: 10px;
+    }
 
+    .posts-list-enter-active,
+    .posts-list-leave-active {
+        transition: all 0.3s ease;
+    }
+
+    .posts-list-enter-from,
+    .posts-list-leave-to {
+        opacity: 0;
+        transform: translateX(130px);
+    }
+
+    .posts-list-move {
+        transition: transform 0.8s ease;
+    }
 </style>
