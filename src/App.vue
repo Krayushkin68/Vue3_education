13078463440e32efd1963d6067442bcb3823a1ae
Krayushkin68 <Krayuskhinml97@gmail.com>
2022-10-03 22:15:50 +0300

    Added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <CustomNavbar/>
+    <router-view/>
 </template>
+
+<script>
+    import CustomNavbar from "@/components/CustomNavbar";
+
+    export default {
+        components: {
+            CustomNavbar
+        }
+    }
+</script>
 
 <style>
 
-nav {
-  padding: 30px;
-}
+    nav {
+        padding: 30px;
+    }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+    nav a.router-link-exact-active {
+        color: #42b983;
+    }
 </style>
---
+++ src/components/CustomNavbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <nav class="navbar navbar-light bg-light navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="/">MyApp</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navContent"
+                    aria-controls="navContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse mx-5" id="navContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link to="/" class="nav-link" :class="{active: isActive('/')}">
+                            Home
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/about" class="nav-link" :class="{active: isActive('/about')}">
+                            About
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "CustomNavbar",
+        methods: {
+            isActive(path) {
+                return this.$route.path === path
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
